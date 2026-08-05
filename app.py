8516d3e7ae79ff9584be97ea4969736cb68d3e16
Omkar Karad <karadomkar21@gmail.com>
2026-08-05 15:58:48 +0530

    Fix history template name

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 app.secret_key = "secret123"
 Base.metadata.create_all(bind=engine)
 
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS") == "True"
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 

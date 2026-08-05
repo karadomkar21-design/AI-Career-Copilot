66c2cb28fe4cf3f8a18727d9cdafc887275e3084
Omkar Karad <karadomkar21@gmail.com>
2026-08-05 15:49:13 +0530

    Fix history template name

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import json
 from ai import analyze_resume
 from itsdangerous import URLSafeTimedSerializer
 from flask_mail import Mail, Message
-
-
+import os
+import dotenv
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -18,8 +18,8 @@ Base.metadata.create_all(bind=engine)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "karadomkar21@gmail.com"
-app.config["MAIL_PASSWORD"] = "xaexofdbitzgdahm"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
 mail = Mail(app)
 

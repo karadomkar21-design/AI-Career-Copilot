a08ad51797c70c459858a59910c62786d301b749
Omkar Karad <karadomkar21@gmail.com>
2026-08-05 23:53:59 +0530

    Describe your changes

--- ai.py
+++ ai.py
@@ -1,18 +1,24 @@
-import requests
-import json
 import os
+import json
 from dotenv import load_dotenv
+from google import genai
 
-load_dotenv()
+load_dotenv("gemini.env")
 
-API_KEY = os.getenv("OPENROUTER_API_KEY")
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+for model in client.models.list():
+    print(model.name)
 def analyze_resume(resume_text, user_goal):
+
     prompt = f"""
 You are a senior software engineer and hiring manager.
 
-Evaluate the resume based on the user's goal.
+Evaluate this resume.
 
-User goal: "{user_goal}"
+User Goal:
+{user_goal}
 
 Return ONLY valid JSON.
 
@@ -23,49 +29,29 @@ Return ONLY valid JSON.
     "interview_questions": []
 }}
 
-resume_text = resume_text[:3000]
 Resume:
+
 {resume_text}
 """
-    print("API KEY FOUND:", API_KEY is not None)
-    print("API KEY PREFIX:", API_KEY[:10] if API_KEY else "None")
+
     try:
-        response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers={
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json",
-    },
-        json={
-        "model": "openai/gpt-oss-20b:free",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "response_format": {
-        "type": "json_object"
-        } 
-    },
-    timeout=(10, 20)
-)
 
-        response.raise_for_status()
+        response = client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=prompt
+        )
 
-        data = response.json()
+        text = response.text.strip()
 
-        content = data["choices"][0]["message"]["content"].strip()
+        text = text.replace("```json", "").replace("```", "").strip()
 
-        # Remove markdown code fences if present
-        content = content.replace("```json", "").replace("```", "").strip()
+        return json.loads(text)
 
-        return json.loads(content)
-    except json.JSONDecodeError as e:
-         print("\nJSON ERROR:", e)
-         print("\nRAW RESPONSE:\n")
-         print(content)
-    raise
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
 
     # except Exception as e:
     #     return {
